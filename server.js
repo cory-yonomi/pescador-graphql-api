810a7d0ab8af0fd2b2a893ec81cc1a1045a01254
ornery-mouse <cory.sorel@gmail.com>
2022-02-07 15:07:41 -0600

    sort function refactored and usgs routes separated

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 // const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const dataRoutes = require('./app/routes/data_routes')
+const usgsRoutes = require('./app/routes/usgs_routes')
 // require middleware
 const errorHandler = require('./lib/error_handler')
 const replaceToken = require('./lib/replace_token')
@@ -68,7 +69,7 @@ app.use(requestLogger)
 // app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(dataRoutes)
-
+app.use(usgsRoutes)
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
 // passed any error messages from them
